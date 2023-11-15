fd92dae420b72f48dbaa6830fdb4c78f78e9b666
ivanlitvin <ivanlitvin51@gmail.com>
2023-11-15 15:33:04 +0700

    update

--- scripts/app.js
+++ scripts/app.js
@@ -12,3 +12,39 @@ for (let elem of buttonsHeaderLink) {
 buttonBurger.onclick = () => alert("Button is unavailable now.");
 
 headerLogo.onclick = () => alert("Button is unavailable now.");
+
+/*
+const testPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Yeah!")
+  }, 2000);
+})
+
+testPromise.then(res => res = "Oh "+res).then(res => alert(res));*/
+
+// const url = "https://jsonplaceholder.typicode.com/todos";
+//
+// const delay = ms => {
+//   return new Promise(r => setTimeout(() => r(), ms));
+// }
+//
+// async function fetchAsync() {
+//   console.log("Fetch started...");
+//   try {
+//     await delay(2000);
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log("Data:", data);
+//
+//   } catch (e) {
+//     console.log("Error: ", e);
+//   }
+//   finally {
+//
+//   }
+// }
+//
+// fetchAsync();
+
+
+
